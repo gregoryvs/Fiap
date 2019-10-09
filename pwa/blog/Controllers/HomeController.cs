be053df01f047a48191f7a2e2b0d1a48ff7214c5
Gregory Victorelli <gregoryvictorelli@gmail.com>
2019-10-09 18:01:23 -0300

    incluindo projeto pwa

---
+++ pwa/blog/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using src.Models;
+
+namespace src.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public JsonResult LatestBlogPosts()
+        {
+            var posts = new List<BlogPost>() 
+            { 
+                new BlogPost { PostId = 1, Title = "xxx", ShortDescription = "xxx" },
+                new BlogPost { PostId = 2, Title = "xxx", ShortDescription = "xxx" }, 
+                new BlogPost { PostId = 3, Title = "xxx", ShortDescription = "xxx" },
+                new BlogPost { PostId = 4, Title = "xxx", ShortDescription = "xxx" },
+                new BlogPost { PostId = 5, Title = "xxx", ShortDescription = "xxx" }
+            };
+            return Json(posts);
+        }
+    }
+}
---
+++ pwa/blog/wwwroot/js/blogService.js
@@ -0,0 +1,15 @@
+﻿define(['./template.js'], function (template) {
+    var blogPostUrl = '/Home/LatestBlogPosts/';
+
+    function loadLatestBlogPosts() {
+        fetch(blogPostUrl)
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                template.appendBlogList(data);
+            });
+    }
+    return {
+        loadLatestBlogPosts: loadLatestBlogPosts
+    }
+});
